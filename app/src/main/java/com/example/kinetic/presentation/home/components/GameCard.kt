c9aaa58a73c2c39325a831e4536eeeb589db8be7
BENJAH <benjahjp@gmail.com>
2023-01-25 17:02:00 +0300

    fix: lazy vertical grid and card

--- app/src/main/java/com/example/kinetic/presentation/home/components/GameCard.kt
+++ app/src/main/java/com/example/kinetic/presentation/home/components/GameCard.kt
@@ -1,6 +1,5 @@
 package com.example.kinetic.presentation.home.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
