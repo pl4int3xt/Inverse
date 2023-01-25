3891e282f6e9637dc9a694840c2680b669a3c68b
BENJAH <benjahjp@gmail.com>
2023-01-25 20:09:34 +0300

    feat: Single genre and platform

--- app/src/main/java/com/example/kinetic/presentation/game_details/components/SingleGenre.kt
+++ app/src/main/java/com/example/kinetic/presentation/game_details/components/SingleGenre.kt
@@ -1,8 +1,35 @@
 package com.example.kinetic.presentation.game_details.components
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun SingleGenre() {
-    
+fun SingleGenre(
+    name: String? = ""
+) {
+    Box(modifier = Modifier
+        .border(
+            width = 1.dp,
+            color = MaterialTheme.colors.primary,
+            shape = RoundedCornerShape(100.dp)
+        )
+        .padding(10.dp)
+    ){
+        if (name != null){
+            Text(
+                text = name,
+                color = MaterialTheme.colors.primary,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.body2
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/kinetic/presentation/game_details/components/SinglePlatform.kt
+++ app/src/main/java/com/example/kinetic/presentation/game_details/components/SinglePlatform.kt
@@ -1,8 +1,42 @@
 package com.example.kinetic.presentation.game_details.components
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun SinglePlatform() {
-
+fun SinglePlatform(
+    icon: Int? = null,
+    name: String? = null
+) {
+    Box(modifier = Modifier
+        .border(
+            width = 1.dp,
+            color = MaterialTheme.colors.primary,
+            shape = RoundedCornerShape(100.dp)
+        )
+        .padding(10.dp)
+    ){
+        if (icon != null){
+            Icon(painter = painterResource(id = icon),
+                contentDescription = "windows")
+        }
+        if (name != null){
+            Text(
+                text = name,
+                color = MaterialTheme.colors.primary,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.body2
+            )
+        }
+    }
 }
