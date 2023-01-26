5fdd0148e381c40e80c105d87534bf27f1b2a9ca
BENJAH <benjahjp@gmail.com>
2023-01-26 13:48:50 +0300

    fix:

--- app/src/main/java/com/example/kinetic/presentation/game_details/components/GamesDetailsScreen.kt
+++ app/src/main/java/com/example/kinetic/presentation/game_details/components/GamesDetailsScreen.kt
@@ -122,7 +122,7 @@ fun GameDetailsScreen(
                                 .height(400.dp)
                         ) {
                             AsyncImage(
-                                contentScale = ContentScale.Inside,
+                                contentScale = ContentScale.None,
                                 model = state.gameDetails?.backgroundImage,
                                 contentDescription = "image"
                             )
