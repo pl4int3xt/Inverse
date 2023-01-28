e50ded891ad55a98f63ed888e9c7c5b3f4f495e7
BENJAH <benjahjp@gmail.com>
2023-01-28 11:12:34 +0300

    fix: Pagination

--- app/src/main/java/com/example/kinetic/presentation/home/HomeScreenState.kt
+++ app/src/main/java/com/example/kinetic/presentation/home/HomeScreenState.kt
@@ -1,8 +1,5 @@
 package com.example.kinetic.presentation.home
 
-import androidx.compose.runtime.MutableState
-import com.example.kinetic.domain.model.GameModel
-
 data class HomeScreenState(
     val isLoading: Boolean = false,
     val isNextLoading: Boolean = false,
