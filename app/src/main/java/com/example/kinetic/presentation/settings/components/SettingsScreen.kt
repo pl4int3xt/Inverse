b34b403b7bb7c3c74ffc0de509786cc1806ce3af
b3njah <benjahjp@gmail.com>
2023-07-06 12:35:52 +0300

    feat: color modes settings

--- app/src/main/java/com/example/kinetic/presentation/settings/components/SettingsScreen.kt
+++ app/src/main/java/com/example/kinetic/presentation/settings/components/SettingsScreen.kt
@@ -1,2 +1,82 @@
 package com.example.kinetic.presentation.settings.components
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Brush
+import androidx.compose.material3.DismissibleDrawerSheet
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsScreen() {
+    Scaffold() {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "Customization",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+            ElevatedButton(
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO*/ }
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Icon(imageVector = Icons.Filled.Brush, contentDescription = "customize")
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Text(
+                        text = "Themes",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(5.dp))
+            ElevatedButton(
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO*/ }) {
+                Column {
+                    Text(
+                        text = "Language",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(text = "(System Default)")
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SettingsPrev() {
+    SettingsScreen()
+}
