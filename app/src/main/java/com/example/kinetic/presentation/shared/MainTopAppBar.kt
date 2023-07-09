97d657c977f38a6ddeef65638e63054bd7c7595b
b3njah <benjahjp@gmail.com>
2023-07-09 15:17:26 +0300

    feat: icon buttons

--- app/src/main/java/com/example/kinetic/presentation/shared/MainTopAppBar.kt
+++ app/src/main/java/com/example/kinetic/presentation/shared/MainTopAppBar.kt
@@ -1,11 +1,6 @@
 package com.example.kinetic.presentation.shared
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -17,7 +12,6 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
