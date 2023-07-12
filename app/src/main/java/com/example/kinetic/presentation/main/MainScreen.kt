66f0e35d4de97beacb5445f1905cd2e0c5c2cc8c
b3njah <benjahjp@gmail.com>
2023-07-12 12:21:19 +0300

    feat: bottom bar height

--- app/src/main/java/com/example/kinetic/presentation/main/MainScreen.kt
+++ app/src/main/java/com/example/kinetic/presentation/main/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.example.kinetic.presentation.main
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.icons.Icons
@@ -8,11 +9,14 @@ import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.kinetic.presentation.navigation.MainNavGraph
 import com.example.kinetic.presentation.screen.Screens
 
