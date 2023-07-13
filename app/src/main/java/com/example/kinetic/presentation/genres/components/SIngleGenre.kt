e6fa91890e7d54aee5979c0c15ad6d3863df0f8b
b3njah <benjahjp@gmail.com>
2023-07-13 10:36:06 +0300

    feat: genre presentation

--- app/src/main/java/com/example/kinetic/presentation/genres/components/SIngleGenre.kt
+++ app/src/main/java/com/example/kinetic/presentation/genres/components/SIngleGenre.kt
@@ -12,11 +12,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.example.kinetic.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,6 +45,7 @@ fun SingleGenre(
             AsyncImage(
                 modifier = Modifier.height(200.dp),
                 model = image,
+                placeholder = painterResource(id = R.drawable.image_loading),
                 contentDescription = "image",
                 contentScale = ContentScale.Crop
             )
