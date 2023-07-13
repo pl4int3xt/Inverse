2ff64f8adda793017f61b985710a75a616d20dde
b3njah <benjahjp@gmail.com>
2023-07-13 09:42:32 +0300

    feat: get genres repo

---
+++ app/src/main/java/com/example/kinetic/domain/use_case/GetGenresUseCase.kt
@@ -0,0 +1,29 @@
+package com.example.kinetic.domain.use_case
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.kinetic.data.remote.dto.Genre
+import com.example.kinetic.domain.repository.GameRepository
+import javax.inject.Inject
+
+class GetGenresUseCase @Inject constructor(
+    private val repository: GameRepository
+) : PagingSource<Int, Genre>() {
+    override fun getRefreshKey(state: PagingState<Int, Genre>): Int? {
+        return null
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Genre> {
+        return try {
+            val currentPage = params.key ?: 1
+            val response = repository.getGenres(currentPage, pageSize = 20)
+            LoadResult.Page(
+                data = response.results,
+                prevKey = if (currentPage == 1) null else -1,
+                nextKey = currentPage.plus(1)
+            )
+        } catch (e: Exception){
+            LoadResult.Error(e)
+        }
+    }
+}
