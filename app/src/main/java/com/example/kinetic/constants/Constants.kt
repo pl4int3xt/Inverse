e4666dd0bc083d59f1a7f1d80b526ac62ce536e5
b3njah <benjahjp@gmail.com>
2023-07-17 10:43:17 +0300

    feat: games list feature

--- app/src/main/java/com/example/kinetic/constants/Constants.kt
+++ app/src/main/java/com/example/kinetic/constants/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     private const val BASE_URL = "https://api.rawg.io/api"
     const val GET_GAMES_URL = "$BASE_URL/games"
     const val GET_GENRES_URL = "$BASE_URL/genres"
+    const val API_KEY = "ddcd58c5aaef4a71981eff6c99e548f4"
 }
--- app/src/main/java/com/example/kinetic/domain/repository/GameRepository.kt
+++ app/src/main/java/com/example/kinetic/domain/repository/GameRepository.kt
@@ -9,5 +9,5 @@ interface GameRepository {
     suspend fun getGamesDetails(gameId: String): GamesDetailsDto
     suspend fun searchGame(searchQuery: String, page: Int, pageSize: Int): GamesDto
     suspend fun getGenres(page:Int, pageSize: Int): GenresResponse
-    suspend fun getGamesByCategory(page: Int, pageSize: Int, category: String): GamesDto
+    suspend fun getGamesByCategory(page: Int, pageSize: Int, genre: String): GamesDto
 }
