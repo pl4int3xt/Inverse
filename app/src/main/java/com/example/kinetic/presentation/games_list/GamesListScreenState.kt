add8f57cdf8242b384f635f3c7f1ab88072f3399
b3njah <benjahjp@gmail.com>
2023-07-19 20:36:26 +0300

    feat: games list

---
+++ app/src/main/java/com/example/kinetic/presentation/games_list/GamesListScreenState.kt
@@ -0,0 +1,6 @@
+package com.example.kinetic.presentation.games_list
+data class SearchScreenState(
+    val isLoading: Boolean = false,
+    val isNextLoading: Boolean = false,
+    val message: String = ""
+)
--- app/src/main/java/com/example/kinetic/presentation/games_list/components/GamesListScreen.kt
+++ app/src/main/java/com/example/kinetic/presentation/games_list/components/GamesListScreen.kt
@@ -5,11 +5,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.kinetic.presentation.games_list.GamesListViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GamesListScreen() {
+fun GamesListScreen(
+    viewModel: GamesListViewModel = hiltViewModel()
+) {
+
     Scaffold {
         LazyColumn(){
 
