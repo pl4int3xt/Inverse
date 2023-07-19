0928f6ce0097c2890ff8a9381bdb2118989ceae1
b3njah <benjahjp@gmail.com>
2023-07-19 20:11:45 +0300

    feat: bottom navigation

--- app/src/main/java/com/example/kinetic/presentation/games_list/components/GamesListScreen.kt
+++ app/src/main/java/com/example/kinetic/presentation/games_list/components/GamesListScreen.kt
@@ -1,4 +1,18 @@
 package com.example.kinetic.presentation.games_list.components
 
-class GamesListScreen {
+import android.annotation.SuppressLint
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GamesListScreen() {
+    Scaffold {
+        LazyColumn(){
+
+        }
+    }
 }
